48a8f1303e0964d2e4f0cf150fa73029f1a9e11f
LizCira <liz.cira.goldstein@gmail.com>
2014-08-23 17:40:18 -0400

    corrected the corr function loop, moved correlation inside the loop

--- corr.R
+++ corr.R
@@ -3,18 +3,29 @@ corr <- function(directory, threshold = 0) {
         files_full <- list.files( directory, full.names=TRUE )
         dir_length <- length(files_full)
 
+        current_record <- data.frame()
         full_dat <- data.frame()
 
+
         for(i in 1:dir_length){
-            full_dat <- rbind(full_dat, read.csv(files_full[i]) )
+            current_record <- read.csv(files_full[i])
+            clean <- current_record[complete.cases(current_record),]
+            count <- length(clean$Date)
+
+            if( count > threshold ){
+                corr <- cor(clean$nitrate, clean$sulfate)
+                full_dat <- rbind(full_dat, corr)
+                }
           }
 
-        clean_data <- full_dat[complete.cases(full_dat),]
-        nitrate <- clean_data$nitrate
-        sulfate <- clean_data$sulfate
-        final <- cor(sulfate, nitrate)
-        print(final)
+  full_dat
 }
+
+      # clean_data <- full_dat[complete.cases(full_dat),]
+      #   nitrate <- clean_data$nitrate
+      #   sulfate <- clean_data$sulfate
+      #   final <- cor(sulfate, nitrate)
+      #   print(final)
 
         ## 'directory' is a character vector of length 1 indicating
         ## the location of the CSV files
